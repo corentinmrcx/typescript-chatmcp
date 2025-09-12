7a9528344752895e6127b7cccff45721e5bb0768
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2025-09-12 08:31:59 +0000

    Utilisation du repository dans la classe ChatModel

--- src/chat/chat.model.ts
+++ src/chat/chat.model.ts
@@ -1,16 +1,30 @@
 import { google } from '@ai-sdk/google';
-import { generateText } from 'ai';
+import { generateText, ModelMessage } from 'ai';
+import { ChatRepository } from './chat.repository';
 
 const MODEL_NAME = 'gemini-2.0-flash';
 
 export class ChatModel {
+    private _chatId : string;
+    static repository : ChatRepository<ModelMessage> = new ChatRepository;
+
+    constructor(){
+        this._chatId = ChatModel.repository.create([]);
+    }
+
+    get id(): string {
+        return this._chatId;
+    }
+    
     async send(prompt: string): Promise<string> {
-        const {text} = await generateText(
+        ChatModel.repository.addMessages(this._chatId, [{ role: 'user', content: prompt }])
+        const { text, response } = await generateText(
             {
                 model: google(MODEL_NAME),
-                prompt: prompt
+                prompt: ChatModel.repository.find(this._chatId)
             }
-        )
+        );
+        ChatModel.repository.addMessages(this._chatId, response.messages)
         return text;
     }
 }
