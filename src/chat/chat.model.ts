84b1be59264566d5131c95bd9c995e87e22191c3
Corentin Marcoux <corentin.marcoux@etudiant.univ-reims.fr>
2025-10-17 08:56:05 +0000

    Création du client

--- src/chat/chat.model.ts
+++ src/chat/chat.model.ts
@@ -1,12 +1,15 @@
 import { google } from '@ai-sdk/google';
-import { generateText, ModelMessage } from 'ai';
+import { generateText, ModelMessage, stepCountIs, ToolSet } from 'ai';
 import { ChatRepository } from './chat.repository';
+import { experimental_createMCPClient as createMCPClient } from 'ai';
+import { Experimental_StdioMCPTransport as StdioClientTransport } from 'ai/mcp-stdio';
 
 const MODEL_NAME = 'gemini-2.0-flash';
 
 export class ChatModel {
     private _chatId : string;
     static repository : ChatRepository<ModelMessage> = new ChatRepository;
+    private static _tools : ToolSet | null = null;
 
     constructor(chatId?: string){
         if (chatId) {
@@ -27,16 +30,28 @@ export class ChatModel {
         ChatModel.repository.addMessages(this._chatId, [{ role: 'user', content: prompt }])
     }
 
-    private createGenerationConfig(): GenerationConfig {
+    private async createGenerationConfig(): Promise<GenerationConfig> {
+        if (ChatModel._tools === null) {
+            const mcpClient = await createMCPClient({
+                transport: new StdioClientTransport({
+                    command: 'node',
+                    args: ['/home/butinfo/mcp-servers/mcp-time/dist/server.js'],
+                }),
+            });
+            ChatModel._tools = await mcpClient.tools();
+        }
         return {
             model: google(MODEL_NAME),
-            messages: ChatModel.repository.find(this._chatId)
+            messages: ChatModel.repository.find(this._chatId),
+            tools: ChatModel._tools, 
+            stopWhen: stepCountIs(10)
         }
     }
 
     async fetchAnswer(): Promise<string> {
-        const { text, response } = await generateText(this.createGenerationConfig());
-        ChatModel.repository.addMessages(this._chatId, response.messages)
+        const config = await this.createGenerationConfig();
+        const { text, response } = await generateText(config);
+        ChatModel.repository.addMessages(this._chatId, response.messages);
         return text;
     }
 }
